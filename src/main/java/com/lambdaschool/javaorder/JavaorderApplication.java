316e140863ec0ad9dcb34b487420400c7c25c46e
Chris King <cking5836@gmail.com>
2020-08-10 14:42:35 -0400

    Initial commit

---
+++ src/main/java/com/lambdaschool/javaorder/JavaorderApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaorderApplication.class, args);
+    }
+
+}
